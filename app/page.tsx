5fb6476b7b5c2b3fea07abb490ecaa5ad4a9b84b
erictli <hi@ericli.io>
2024-01-01 11:18:34 -0500

    new backgrounds

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-// Add better way to manage sprite states in the spritesheet (e.g. rows and columns)
 // Apply the direction to standing too
 // Vertical alignment for big monitors
-// When reach the end of the page no longer aligned with page center
 // New sprites
 // Animation when sprite intersects text
 // Down arrow links (crouch, animate teleportation, open new tab)
 // Special mouse cursor
-// Character animates in by walking in from the left
+// Change colors in different sections (requires moving gradients to the page)
 
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
+import { Menu, HomeIcon } from "lucide-react";
 
 const spritesheet = "/images/spritesheet-eric.svg";
 const spriteWidth = 64 * 2;
@@ -21,25 +20,25 @@ const walkingFrameRate = 150; // Time in milliseconds for each walking frame
 const moveSpeed = 5; // Pixels to move per frame
 const jumpVelocity = -12; // Initial velocity for the jump, negative for upward
 const gravity = 0.6; // Gravity applied to the character
-const textBlocks = [
-  "Hi, I'm Eric.",
-  "I’m the co-founder of Versive, an AI-first survey platform.",
-  "I’m a self-taught designer, developer, and product manager.",
-  "I've worked at tiny startups and public companies. Most recently I was at Vareto, Uber, and Bread.",
-  "I’m originally from the Chicago suburbs and currently live in Brooklyn, NY.",
-  "Thanks for walking with me.",
-];
-const secondBlockPosition =
-  typeof window !== "undefined" ? window.innerWidth / 2 + 55 + 540 - 120 : 0; // Position of the second text block 540px from the first block with a 120px buffer
 const textPositions = [
   0,
-  secondBlockPosition,
-  secondBlockPosition + 860,
-  secondBlockPosition + 860 * 2,
-  secondBlockPosition + 860 * 3,
-  secondBlockPosition + 860 * 4,
+  1434+560/2-160-320, // Need to subtract the width of the previous text
+  1434+560/2+860-320*2,
+  1434+560/2+860*2-320*2,
+  1434+560/2+860*3-320*2,
+  1434+560/2+860*4-320*2,
+  1434+560/2+860*5-320*2,
 ];
-const backgroundWidth = 96 * 3 + 1434 + 1334 + 1170 + 1334;
+const textPositionTriggers = [
+  0,
+  1434+560/2-160, // Need to subtract the width of the previous text
+  1434+560/2+860,
+  1434+560/2+860*2,
+  1434+560/2+860*3,
+  1434+560/2+860*4,
+  1434+560/2+860*5,
+];
+const backgroundWidth = 96 * 3 + 1434 + 1334 + 1170 + 1334 + 1334 + 1200;
 
 // Define types for your state
 type SpriteState = {
@@ -50,10 +49,6 @@ type SpriteState = {
   verticalPosition: number;
   verticalVelocity: number;
   isJumping: boolean;
-};
-
-type TextVisibilityState = {
-  visibleSections: boolean[];
 };
 
 export default function Home() {
@@ -69,7 +64,7 @@ export default function Home() {
     useState<SpriteState["verticalVelocity"]>(0);
   const [isJumping, setIsJumping] = useState<SpriteState["isJumping"]>(false);
   const [animationTriggers, setAnimationTriggers] = useState<boolean[]>(
-    new Array(textBlocks.length).fill(false),
+    new Array(textPositionTriggers.length).fill(false),
   );
 
   const moveDirection = useRef(0); // 1 for right, -1 for left
@@ -200,7 +195,7 @@ export default function Home() {
 
   // Effect to trigger animations based on the sprite's position
   useEffect(() => {
-    textPositions.forEach((xPosition, index) => {
+    textPositionTriggers.forEach((xPosition, index) => {
       if (position >= xPosition && !animationTriggers[index]) {
         setAnimationTriggers((prevTriggers) => {
           const newTriggers = [...prevTriggers];
@@ -221,10 +216,10 @@ export default function Home() {
     <main className="flex items-center justify-center bg-white p-4">
       <div
         ref={containerRef}
-        className="relative h-[calc(100dvh-32px)] w-[calc(100dvw-32px)] overflow-hidden bg-black"
+        className={`relative h-[calc(100dvh-32px)] w-[calc(100dvw-32px)] overflow-hidden transition-colors duration-5000 ${animationTriggers[1] && !animationTriggers[2] ? 'bg-teal-900' : 'bg-black' }`}
       >
         <div className="relative" style={{ width: backgroundWidth + "px" }}>
-          <div className="absolute top-[calc(85dvh-560px-12px)] left-0 flex items-center gap-24 pointer-events-none select-none opacity-0 animate-fadeInBackground">
+          <div className="absolute top-[calc(85dvh-560px-12px)] left-0 flex items-center pointer-events-none select-none opacity-0 animate-fadeInBackground">
             <Image
               src="/images/background-1.png"
               alt="Background"
@@ -233,6 +228,49 @@ export default function Home() {
               width={1434}
               quality={100}
               className=""
+            />
+            <div className="flex-none h-[560px] w-[560px] mr-[240px] flex items-center justify-center">
+              <Image
+                src="/images/versive.svg"
+                priority
+                height={440}
+                width={440}
+                alt="Versive Logo"
+                className={`animate-spin !duration-8000 mb-4 transition-opacity ${animationTriggers[1] ? 'opacity-[8%]' : 'opacity-5'}`}
+              />
+            </div>
+            <div className="flex-none h-[560px] w-[880px] bg-white/[8%] rounded-md mr-10">
+              <div className="w-full border-b border-white/5 flex items-center">
+                <div className="flex items-center gap-2 p-3 h-8 border-r border-white/5 ">
+                  <div className="h-[10px] w-[10px] rounded-full border border-white/[8%]"></div>
+                  <div className="h-[10px] w-[10px] rounded-full border border-white/[8%]"></div>
+                  <div className="h-[10px] w-[10px] rounded-full border border-white/[8%]"></div>
+                </div>
+                <div className="h-8 flex items-center justify-center p-3 border-r border-white/5">
+                  <HomeIcon size={12} className=" fill-white opacity-20" />
+                </div>
+                <div className="h-8 flex gap-[6px] items-center justify-center p-3 text-[9px] border-r border-white/5">
+                  <div className="h-3 w-3 bg-sky-500/30 rounded-sm"></div>
+                  ericli.io
+                </div>
+              </div>
+            </div>
+            <Image
+              src="/images/background-office.png"
+              alt="Background"
+              priority
+              height={560}
+              width={1170}
+              quality={100}
+              className=""
+            />
+                        <Image
+              src="/images/background-campus.png"
+              alt="Background"
+              priority
+              height={560}
+              width={1170}
+              quality={100}
             />
             <Image
               src="/images/background-2.png"
@@ -250,31 +288,73 @@ export default function Home() {
               width={1170}
               quality={100}
             />
-            <Image
-              src="/images/background-2.png"
-              alt="Background"
-              priority
-              height={560}
-              width={1334}
-              quality={100}
-            />
           </div>
 
-          <div className="relative left-[calc(100dvw/2-16px-55px)] top-[calc(100dvh/2-80px)] flex items-center gap-[540px] text-[25px] leading-snug">
-            {textBlocks.map((text, index) => (
-              <div
-                key={index}
-                className={`font-display max-w-[320px] opacity-0 ${
-                  animationTriggers[index] ? "animate-fadeInText" : ""
-                }`}
-              >
-                {text}
-              </div>
-            ))}
+          <div className="relative top-[calc(100dvh/2-80px)] flex items-center text-[25px] leading-snug">
+            <div
+              className={`relative font-display w-[320px] opacity-0 left-[calc(100dvw/2-16px-160px)] text-center ${
+                animationTriggers[0] ? "animate-fadeInText" : ""
+              }`}
+            >
+              Hi, I'm Eric.
+            </div>
+
+            <div
+              className={`relative font-display w-[320px] opacity-0 text-center ${
+                animationTriggers[1] ? "animate-fadeInText" : ""
+              }`}
+              style={{ left: textPositions[1] + "px" }}
+            >
+              I’m the co-founder of Versive, an AI-first survey platform.
+            </div>
+            <div
+              className={`relative font-display w-[320px] opacity-0 text-center border border-white/10 ${
+                animationTriggers[2] ? "animate-fadeInText" : ""
+              }`}
+              style={{ left: textPositions[2] + "px" }}
+            >
+              I’m a self-taught designer, developer, and product manager.
+              <div className="absolute text-[10px] font-sans opacity-30 -top-4">Frame 1</div>
+            </div>
+            <div
+              className={`relative font-display w-[320px] opacity-0 ${
+                animationTriggers[3] ? "animate-fadeInText" : ""
+              }`}
+              style={{ left: textPositions[3] + "px" }}
+            >
+              I've worked at tiny startups and public companies. Most recently I
+              was at Vareto, Uber, and Bread.
+            </div>
+            <div
+              className={`relative font-display w-[320px] opacity-0 ${
+                animationTriggers[4] ? "animate-fadeInText" : ""
+              }`}
+              style={{ left: textPositions[4] + "px" }}
+            >
+              In a past life, I worked in finance and studied economics at the
+              University of Chicago.
+            </div>
+            <div
+              className={`relative font-display w-[320px] opacity-0 ${
+                animationTriggers[5] ? "animate-fadeInText" : ""
+              }`}
+              style={{ left: textPositions[5] + "px" }}
+            >
+              I’m originally from the Chicago suburbs and currently live in
+              Brooklyn, NY.
+            </div>
+            <div
+              className={`relative font-display w-[320px] opacity-0 ${
+                animationTriggers[6] ? "animate-fadeInText" : ""
+              }`}
+              style={{ left: textPositions[6] + "px" }}
+            >
+              Thanks for walking with me.
+            </div>
           </div>
           <div
             id="sprite"
-            className="mt-[calc(85dvh-192px)] animate-fadeInSprite opacity-0"
+            className={`mt-[calc(85dvh-192px)] animate-fadeInSprite opacity-0 ${animationTriggers[2] && !animationTriggers[3] ? 'border border-white/10' : '' }}`}
             style={{
               position: "absolute",
               left: `${position}px`,
@@ -284,9 +364,26 @@ export default function Home() {
               background: `url(${spritesheet}) ${backgroundPositionX}px ${backgroundPositionY}px`,
               transform: moveDirection.current === -1 ? "scaleX(-1)" : "none", // Flips the sprite when moving left
             }}
-          ></div>
+          >
+            {/* Move sprite border to a separate component with fixed height and width
+            Add label for Layer 1 */}
+          </div>
         </div>
-        <div className="absolute bottom-5 text-xs left-1/2 -translate-x-1/2 text-neutral-500 opacity-0 animate-fadeInControls">User the arrow keys to move</div>
+        <div className="fixed top-9 right-9 text-xs opacity-0 animate-fadeInMenu">
+          <Menu size={20} className="stroke-[1.5]" />
+          {/* 
+            Last updated
+            Text mode
+            Linkedin
+            Readcv
+            Email
+            Versive
+
+           */}
+        </div>
+        <div className="fixed bottom-9 text-xs left-1/2 -translate-x-1/2 text-neutral-500 opacity-0 animate-fadeInControls">
+          User the arrow keys to move
+        </div>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -17,6 +17,14 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: [
+          "var(--font-inter), ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+        ],
+        display: [
+          "var(--font-mondwest), var(--font-inter), ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+        ],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -66,12 +74,72 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeIn: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        fadeInUp: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(24px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        fadeInDown: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-16px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        fadeInUpControls: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(8px) translateX(-50%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0) translateX(-50%)",
+          },
+        },
+        panIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-480px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        fadeInText: "fadeInUp 1s ease-out forwards 0.4s",
+        fadeInBackground:
+          "panIn 2.5s cubic-bezier(0.33, 1, 0.68, 1) forwards 1.9s",
+        fadeInSprite: "fadeIn 0.5s ease-out forwards 4.5s",
+        fadeInControls: "fadeInUpControls 0.5s ease-out forwards 5.0s",
+        fadeInMenu: "fadeInDown 0.5s ease-out forwards 5.5s",
+      },
+      transitionDuration: {
+        1500: "1500ms",
+        2000: "2000ms",
+        5000: "5000ms",
+        8000: "8000ms",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
